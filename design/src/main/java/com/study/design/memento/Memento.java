304f80bb2af5c2bc9e563da56e3b0bdbc4c886f6
李鹏 <peng.li@e-jiazi.com>
2017-10-11 14:53:24 +0800

    memento 备忘录模式

---
+++ design/src/main/java/com/study/design/memento/Memento.java
@@ -0,0 +1,20 @@
+package com.study.design.memento;
+
+/**
+ * Created by Administrator on 2017/10/11.
+ */
+public class Memento {
+    private String value;
+
+    public Memento(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
---
+++ design/src/main/java/com/study/design/memento/Storage.java
@@ -0,0 +1,20 @@
+package com.study.design.memento;
+
+/**
+ * Created by Administrator on 2017/10/11.
+ */
+public class Storage {
+    private Memento memento;
+
+    public Storage(Memento memento) {
+        this.memento = memento;
+    }
+
+    public Memento getMemento() {
+        return memento;
+    }
+
+    public void setMemento(Memento memento) {
+        this.memento = memento;
+    }
+}
